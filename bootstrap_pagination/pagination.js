2f90e797be35771e27cdcba61925189726fb65cf
uygurran2008@126.com <uygurjan2008@126.com>
2015-06-24 10:49:08 +0800

    c

--- bootstrap_pagination/pagination.js
+++ bootstrap_pagination/pagination.js
@@ -1,37 +1,59 @@
-var data_on_page = [];
-function bootstrap_pagination(data, pagesize, target1, target2) {
-	//col_tmp   html array
-//	bootstrap_pagination(col_tmp, 20, $(
-	//		".page", root), $(".content", root));
 
-	// target = $(".pagination");
+function pagination(data, pagesize, target1, target2) {
+	var data_on_page = [];
+	var a_style = "position: relative;float: left;"
+			+ "padding: 6px 12px; margin-left: -1px;"
+			+ "line-height: 1.42857143;color: #337ab7;"
+			+ "text-decoration: none;  background-color: #fff;"
+			+ "border: 1px solid #ddd;";
+	var li_style = "display: inline;";
 	var dt = data_page(data, pagesize);
 	var tmps = "<div class='col-md-12' style='/*border:1px solid;border-radius:10px;*/'>"
-			+ "<ul class='pagination' style='margin:0px;'>";
-	var p_before = "<li><a class='previous btn btn-default'>&laquo;上5页</a></li>";
-	var p_next = "<li><a class='next btn btn-default'>下5页&raquo;</a></li>";
-	var p_goto = "<li><a class='btn btn-default go'>GO</a><input type='text' class='p_number' style='width:60px;height:30px;' />"
+			+ "<ul class='pagination' style='margin:0px;margin:0px;display: inline-block;"
+			+ "padding-left: 0;margin: 20px 0;border-radius: 4px;'>";
+	var p_before = "<li style=\"" + li_style + "\"><a style=\"" + a_style
+			+ "\" class='previous btn btn-default'>&laquo;上5页</a></li>";
+	var p_next = "<li style=\"" + li_style + "\"><a style=\"" + a_style
+			+ "\"  class='next btn btn-default'>下5页&raquo;</a></li>";
+	var p_goto = "<li style=\""
+			+ li_style
+			+ "\"><a style=\""
+			+ a_style
+			+ "\"  class='btn btn-default go'>GO</a><input type='text' class='p_number' style='width:60px;height:30px;' />"
 			+ "</li>";
-	var p_all = "<li><span>总共" + dt.length + "页</span></li>";
-	var p_end = "<li><span class=\"page_end\" >最后一页</span></li>";
-	var p_start = "<li><span class=\"page_start\">第一页</span></li>";
+	var p_all = "<li style=\"" + li_style + "\"><span>总共" + dt.length
+			+ "页</span></li>";
+	var p_end = "<li style=\"" + li_style
+			+ "\"><span class=\"page_end\" >最后一页</span></li>";
+	var p_start = "<li style=\"" + li_style
+			+ "\"><span class=\"page_start\">第一页</span></li>";
 
 	var pages_num = "";
 	if (dt.length <= 5) {
 		for (var i = 0; i < dt.length; i++) {
 			if (i == 0) {
-				pages_num += "<li><a page=\"1\" class=\"selected\" style=\"background: rgb(32, 163, 153);\">1</a></li>";
+				pages_num += "<li style=\""
+						+ li_style
+						+ "\"><a style=\""
+						+ a_style
+						+ "\"  page=\"1\" class=\"selected\" style=\"background: rgb(32, 163, 153);\">1</a></li>";
 			} else {
-				pages_num += "<li><a page=" + (i + 1) + " class=\"pages\">"
+				pages_num += "<li style=\"" + li_style + "\"><a style=\""
+						+ a_style + "\"  page=" + (i + 1) + " class=\"pages\">"
 						+ (i + 1) + "</a></li>";
 			}
 		}
 	} else {
 		for (var i = 0; i < 5; i++) {
 			if (i == 0) {
-				pages_num += "<li><a page=\"1\" class=\"selected\" style=\"background: rgb(32, 163, 153);\">1</a></li>";
+				pages_num += "<li style=\""
+						+ li_style
+						+ "\"><a style=\""
+						+ a_style
+						+ "\"  page=\"1\" class=\"selected\" style=\"background: rgb(32, 163, 153);\">1</a></li>";
 			} else {
-				pages_num += "<li><a page=" + (i + 1) + " class=\"\">"
+				pages_num += "<li style=\"" + li_style + "\"><a style=\""
+						+ a_style + "\"  page=" + (i + 1) + " class=\"\">"
 						+ (i + 1) + "</a></li>";
 			}
 		}
@@ -41,12 +63,7 @@ function bootstrap_pagination(data, pagesize, target1, target2) {
 			+ "</ul></div>";
 	target1.html(page_html);
 
-	$(".page_start").click(function() {
-
-	});
-	$(".page_end").click(function() {
-
-	});
+	
 
 	var cl = $(".pagination li a[class]");
 	var selected;
@@ -56,6 +73,7 @@ function bootstrap_pagination(data, pagesize, target1, target2) {
 		}
 	}
 	target2.html(dt[0]);
+	add_my_event();
 
 	$("ul li .next", target1).click(
 			function() {
@@ -103,8 +121,9 @@ function bootstrap_pagination(data, pagesize, target1, target2) {
 				}
 				data_on_page = [];
 				data_on_page = dt[sd - 1];
-				// console.log("data_on_page" + sd + ":", data_on_page);
+				
 				target2.html(data_on_page);
+				add_my_event();
 			});
 
 	$("ul li .previous", target1).click(
@@ -147,8 +166,9 @@ function bootstrap_pagination(data, pagesize, target1, target2) {
 				}
 				data_on_page = [];
 				data_on_page = dt[sd - 1];
-				// console.log("data_on_page" + sd + ":", data_on_page);
+				
 				target2.html(data_on_page);
+				add_my_event();
 			});
 
 	$(".pagination li a[page]", target1).click(function() {
@@ -169,10 +189,23 @@ function bootstrap_pagination(data, pagesize, target1, target2) {
 		}
 		data_on_page = [];
 		data_on_page = dt[pagenumber - 1];
-		// console.log("data_on_page" + pagenumber + ":", data_on_page);
+		
 		target2.html(data_on_page);
+		add_my_event();
 	});
-
+	
+	
+	function add_my_event(){
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
 }
 
 function data_page(d, pagesize) {
@@ -200,4 +233,3 @@ function data_page(d, pagesize) {
 	}
 	return dt;
 }
-
